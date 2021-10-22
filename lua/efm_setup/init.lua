70a03c686040da5d6a0a4b87a3c14f64d9148127
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-10-22 20:55:46 -0300

    Allow override configuration

--- lua/efm_setup/init.lua
+++ lua/efm_setup/init.lua
@@ -1,8 +1,10 @@
 local M = {}
 
-function M.get_opts()
-  local config = require("efm_setup.configs")
-  return require("efm_setup.efm_setup").get_language_server_options(config)
+function M.get_opts(config)
+  config = config  or {}
+  local default_config = require("efm_setup.configs")
+  local final_config = vim.tbl_extend("force", default_config, config)
+  return require("efm_setup.efm_setup").get_language_server_options(final_config)
 end
 
 return M
