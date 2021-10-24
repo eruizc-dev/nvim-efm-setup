564516142b54d69c8c786479cba515ccbd96f998
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-10-24 01:16:36 -0300

    Add utils

--- lua/efm_setup/utils.lua
+++ lua/efm_setup/utils.lua
@@ -17,4 +17,18 @@ function utils.get_unique(list, key)
   return arr
 end
 
+function utils.tbl_sublist(list, start, elements)
+  local result = {}
+  for i = start or 1, elements or #list, 1 do
+    result[i + 1 - start] = list[i]
+  end
+  return result
+end
+
+function utils.split_cmd(command)
+  local cmd = command[1]
+  local args = utils.tbl_sublist(command, 2)
+  return cmd, args
+end
+
 return utils
